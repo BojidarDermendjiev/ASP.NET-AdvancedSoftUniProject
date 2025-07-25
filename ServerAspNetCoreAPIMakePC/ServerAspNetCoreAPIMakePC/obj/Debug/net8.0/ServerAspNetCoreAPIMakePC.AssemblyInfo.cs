d82ffdc3cfd1227f33a5c4a2fd5b7bbf301b1420
Bojidar Dermendjiev <bojidardermendjiew@abv.bg>
2025-07-25 16:41:37 +0300

    Refactoring business logical[A

--- ServerAspNetCoreAPIMakePC/ServerAspNetCoreAPIMakePC/obj/Debug/net8.0/ServerAspNetCoreAPIMakePC.AssemblyInfo.cs
+++ ServerAspNetCoreAPIMakePC/ServerAspNetCoreAPIMakePC/obj/Debug/net8.0/ServerAspNetCoreAPIMakePC.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerAspNetCoreAPIMakePC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+335c2aa1da9018a5823df42318bcba0b262a7424")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8eb3cb315fe671cd209f0323f6e5667d00ade4cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerAspNetCoreAPIMakePC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerAspNetCoreAPIMakePC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
