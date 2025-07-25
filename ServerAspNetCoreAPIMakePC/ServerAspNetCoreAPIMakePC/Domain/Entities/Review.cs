659130c2ba591af9bf93b39c8444dfa9f12c034d
Bojidar Dermendjiev <bojidardermendjiew@abv.bg>
2025-07-25 16:38:31 +0300

    Refactoring business logical

--- ServerAspNetCoreAPIMakePC/ServerAspNetCoreAPIMakePC/Domain/Entities/Review.cs
+++ ServerAspNetCoreAPIMakePC/ServerAspNetCoreAPIMakePC/Domain/Entities/Review.cs
@@ -7,8 +7,6 @@
         public User User { get; set; } = null!;
         public int? ProductId { get; set; }
         public Product? Product { get; set; } 
-        public int? TutorialId { get; set; }
-        public Tutorial? Tutorial { get; set; }
         public int Rating { get; set; } 
         public string Comment { get; set; } = null!;
         public DateTime Date { get; set; }
--- ServerAspNetCoreAPIMakePC/ServerAspNetCoreAPIMakePC/obj/Debug/net8.0/ServerAspNetCoreAPIMakePC.AssemblyInfo.cs
+++ ServerAspNetCoreAPIMakePC/ServerAspNetCoreAPIMakePC/obj/Debug/net8.0/ServerAspNetCoreAPIMakePC.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerAspNetCoreAPIMakePC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85ddcb83bb7ab7b95d6454989fc087d3e179037d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86afe77c4d28c3387ab3649d4d62185b95fec4f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerAspNetCoreAPIMakePC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerAspNetCoreAPIMakePC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
