9bf4c4c8ecdbd0b86418f0e7be547e13a1e52a11
Bojidar Dermendjiev <bojidardermendjiew@abv.bg>
2025-07-26 09:48:23 +0300

    refactor: added PlatformFeedback and updated related infrastructure

--- ServerAspNetCoreAPIMakePC/ServerAspNetCoreAPIMakePC/obj/Debug/net8.0/ServerAspNetCoreAPIMakePC.AssemblyInfo.cs
+++ ServerAspNetCoreAPIMakePC/ServerAspNetCoreAPIMakePC/obj/Debug/net8.0/ServerAspNetCoreAPIMakePC.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerAspNetCoreAPIMakePC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72b2c904bbb33ed279a4b9784055cd9eb152ddbb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67a5b36977134aade6bdcb530e8e3843bcda4122")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerAspNetCoreAPIMakePC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerAspNetCoreAPIMakePC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
